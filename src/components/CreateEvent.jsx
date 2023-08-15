250fe75ee70922a52e452780d5763d15a62102dd
T_Higgins <70584458+Th876@users.noreply.github.com>
2023-08-15 08:00:02 -0400

    Update CreateEvent.jsx

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -50,7 +50,7 @@ const CreateEvent = () => {
       <div className='w-full max-w-md pb-12'>
         <div className='mb-4'>
             
-            <Link to='/dashboard' className='inline-block bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600 mb-10 mt-10 -ml-60'>
+            <Link to='/' className='inline-block bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600 mb-10 mt-10 -ml-60'>
               Back to Dashboard
             </Link>
           </div>
